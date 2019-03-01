807fc5ccfcf1eafd73a0b7649bc688dff078ba7e
github username <suoarik@gmail.com>
2019-03-01 15:16:17 +0000

    added premier league winners 2018-1994

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -15,7 +15,7 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            premierLeagueMatches: undefined
+            premierLeagueWinners: undefined
         }
     }
 
@@ -26,6 +26,20 @@ class Home extends Component {
     }
 
     componentDidMount() {
+        let data = []
+        unirest.get("https://api.football-data.org/v2/competitions/PL/")
+            .header("X-Auth-Token", config.key)
+            .end((result) => {
+                data.push(result.body)
+                //extract some more data here about previous winners see API documentations lots of info
+                this.setState({ premierLeagueWinners: data[0].seasons })
+            })
+    }
+
+    mapThroughWinners = (array) => {
+        const premierLeagueWinners = array.filter(winner => winner.winner !== null)
+        console.log(premierLeagueWinners)
+        return premierLeagueWinners.map(winner => <LeagueTable winner={winner} />)
 
     }
 
@@ -37,8 +51,11 @@ class Home extends Component {
         return ( 
             <React.Fragment>
                 <Banner />
-                <LeagueTable />
-
+                <h1>Premier League Winners</h1>
+                {
+                    this.state.premierLeagueWinners &&
+                    this.mapThroughWinners([...this.state.premierLeagueWinners])
+                    }
             </React.Fragment>
             )
     }
